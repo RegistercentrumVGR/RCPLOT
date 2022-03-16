56bd8ae40ecd9842c8d639a322f2c9012c310dbd
Rikard Isaksson <soppen@users.noreply.github.com>
2022-03-16 15:32:46 +0100

    Merge files (#4)
    * merge data documentation into two files
    
    * Write new save function with more output options
    
    * Merge documentation - document
    
    * document rcsave and remove slrsave
    
    * merge utility-functions in utils.R

---
+++ R/slr_data.R
@@ -0,0 +1,191 @@
+#' Swedish average pain VAS datasets
+#'
+#' datasets containing Swedish average pain VAS for four year periods:
+#' 2008/2009, 2010/2011, 2012/2013, and 2014/2015. Data set used for
+#' prom_trends() examples.
+#'
+#' @format A data frame with 1 row and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{r2008_2009}{Swedish average
+#'   pain VAS for 2008/2009, numeric} \item{r2010_2011}{Swedish average pain VAS
+#'   for 2010/2011, numeric} \item{r2012_2013}{Swedish average pain VAS for
+#'   2012/2013, numeric} \item{r2014_2015}{Swedish average pain VAS for
+#'   2014/2015, numeric} }
+#' @keywords SLR pain VAS
+#' @name pain_vas
+"pain_riket"
+
+#' Observed pain VAS dataset
+#'
+#' A dataset containing observed pain VAS for four year periods: 2008/2009,
+#' 2010/2011, 2012/2013, and 2014/2015. Data set used for prom_trends()
+#' examples.
+#' @format A data frame with 68 rows and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{pain2008_2009}{observed
+#'   pain VAS mean for 2008/2009, numeric} \item{pain2010_2011}{observed pain
+#'   VAS mean for 2010/2011, numeric} \item{pain2012_2013}{observed pain VAS
+#'   mean for 2012/2013, numeric} \item{pain2014_2015}{observed pain VAS mean
+#'   for 2014/2015, numeric} }
+#' @keywords SLR pain VAS
+#' @name pain_vas
+"pain_obs"
+
+#' Expected pain VAS dataset
+#'
+#' A dataset containing expected pain VAS calculated with a linear model for
+#' four year periods: 2008/2009, 2010/2011, 2012/2013, and 2014/2015. Data set
+#' used for prom_trends() examples.
+#'
+#' @format A data frame with 68 rows and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{res2008_2009}{expected pain
+#'   VAS mean for 2008/2009, numeric} \item{res2010_2011}{expected pain VAS mean
+#'   for 2010/2011, numeric} \item{res2012_2013}{expected pain VAS mean for
+#'   2012/2013, numeric} \item{res2014_2015}{expected pain VAS mean for
+#'   2014/2015, numeric} }
+#' @keywords SLR pain VAS
+#' @name pain_vas
+"pain_exp"
+
+
+#' Swedish average EQ VAS dataset
+#'
+#' A dataset containing Swedish average EQ VAS for four year periods:
+#' 2008/2009, 2010/2011, 2012/2013, and 2014/2015. Data set used for
+#' prom_trends() examples.
+#'
+#' @format A data frame with 1 row and 5 variables:
+#' \describe{
+#'   \item{hospital}{hospital name, character}
+#'   \item{r2008_2009}{Swedish average EQ VAS for 2008/2009, numeric}
+#'   \item{r2010_2011}{Swedish average EQ VAS for 2010/2011, numeric}
+#'   \item{r2012_2013}{Swedish average EQ VAS for 2012/2013, numeric}
+#'   \item{r2014_2015}{Swedish average EQ VAS for 2014/2015, numeric}
+#' }
+#' @keywords SLR EQ VAS
+#' @name eq_vas
+"eq_vas_riket"
+
+#' Observed EQ VAS dataset
+#'
+#' A dataset containing observed EQ VAS for four year periods: 2008/2009,
+#' 2010/2011, 2012/2013, and 2014/2015. Data set used for prom_trends()
+#' examples.
+#'
+#' @format A data frame with 68 rows and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{eqvas2008_2009}{observed EQ
+#'   VAS mean for 2008/2009, numeric} \item{eqvas2010_2011}{observed EQ VAS mean
+#'   for 2010/2011, numeric} \item{eqvas2012_2013}{observed EQ VAS mean for
+#'   2012/2013, numeric} \item{eqvas2014_2015}{observed EQ VAS mean for
+#'   2014/2015, numeric} }
+#' @keywords SLR EQ VAS
+#' @name eq_vas
+"eq_vas_obs"
+
+#' Expected EQ VAS dataset
+#'
+#' A dataset containing expected EQ VAS calculated with a linear model for four
+#' year periods: 2008/2009, 2010/2011, 2012/2013, and 2014/2015. Data set used
+#' for prom_trends() examples.
+#'
+#' @format A data frame with 68 rows and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{res2008_2009}{expected EQ
+#'   VAS mean for 2008/2009, numeric} \item{res2010_2011}{expected EQ VAS mean
+#'   for 2010/2011, numeric} \item{res2012_2013}{expected EQ VAS mean for
+#'   2012/2013, numeric} \item{res2014_2015}{expected EQ VAS mean for 2014/2015,
+#'   numeric} }
+#' @keywords SLR EQ VAS
+#' @name eq_vas
+"eq_vas_exp"
+
+#' Swedish average satisfaction VAS dataset
+#'
+#' A dataset containing Swedish average satisfaction VAS for four year periods:
+#' 2008/2009, 2010/2011, 2012/2013, and 2014/2015. Data set used for
+#' prom_trends() examples.
+#'
+#' @format A data frame with 1 row and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{r2008_2009}{Swedish average
+#'   satisfaction VAS for 2008/2009, numeric} \item{r2010_2011}{Swedish average
+#'   satisfaction VAS for 2010/2011, numeric} \item{r2012_2013}{Swedish average
+#'   satisfaction VAS for 2012/2013, numeric} \item{r2014_2015}{Swedish average
+#'   satisfaction VAS for 2014/2015, numeric} }
+#' @keywords SLR satisfaction-VAS
+#' @name satisfaction_vas
+"satis_riket"
+
+#' Observed satisfaction VAS dataset
+#'
+#' A dataset containing observed satisfaction VAS for four year periods:
+#' 2008/2009, 2010/2011, 2012/2013, and 2014/2015. Data set used for
+#' prom_trends() examples.
+#'
+#' @format A data frame with 68 rows and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{sati2008_2009}{observed
+#'   satisfaction VAS mean for 2008/2009, numeric} \item{sati2010_2011}{observed
+#'   satisfaction VAS mean for 2010/2011, numeric} \item{sati2012_2013}{observed
+#'   satisfaction VAS mean for 2012/2013, numeric} \item{sati2014_2015}{observed
+#'   satisfaction VAS mean for 2014/2015, numeric} }
+#' @keywords SLR satisfaction-VAS
+#' @name satisfaction_vas
+"satis_obs"
+
+#' Expected satisfaction VAS dataset
+#'
+#' A dataset containing expected satisfaction VAS calculated with a linear model
+#' for four year periods: 2008/2009, 2010/2011, 2012/2013, and 2014/2015. Data
+#' set used for prom_trends() examples.
+#'
+#' @format A data frame with 68 rows and 5 variables: \describe{
+#'   \item{hospital}{hospital name, character} \item{res2008_2009}{expected
+#'   satisfaction VAS mean for 2008/2009, numeric} \item{res2010_2011}{expected
+#'   satisfaction VAS mean for 2010/2011, numeric} \item{res2012_2013}{expected
+#'   satisfaction VAS mean for 2012/2013, numeric} \item{res2014_2015}{expected
+#'   satisfaction VAS mean for 2014/2015, numeric} }
+#' @keywords SLR satisfaction-VAS
+#' @name satisfaction_vas
+"satis_exp"
+
+#' Normalized compass dataset
+#'
+#' A dataset containing normalized hospital means for 9 different variables.
+#' This dataset was used in 2016 annual report. The dataset is the normalized
+#' version of compass_data, normalized with norm_compass(). Dataset is used for
+#' compass() and compass_riket() examples.
+#'
+#' @format A data frame with 68 rows and 9 variables:
+#'   \describe{
+#'     \item{Unit}{hospital name, ordered factor}
+#'     \item{Satisfaction}{normalized satisfaction mean, numeric}
+#'   \item{Pain reduction}{normalized pain reduction before operation and
+#'     1 year after (numeric)}
+#'   \item{Gain in EQ5D}{normalized gain in EQ5D-index before operation and 1 year after (numeric)}
+#'   \item{AE90}{normalized adverse events within 90 days, numeric}
+#'   \item{Coverage}{normalized coverage, numeric}
+#'   \item{Reop 2 years}{normalized reoperation within 2 years, numeric}
+#'   \item{Implant 5 years}{normalized implant survival within 5 years, numeric}
+#'   \item{Implant 10 years}{normalized implant survival within 10 years, numeric}
+#'}
+#' @keywords SLR compass
+#' @name compass
+"norm_compass_data"
+
+#' Compass dataset
+#'
+#' A dataset containing hospital means for 9 different variables. This dataset
+#' was used in 2016 annual report. Used for norm_compass() example, where
+#' outcome is used for compass() and compass_riket() examples.
+#'
+#' @format A data frame with 68 rows and 9 variables:
+#' \describe{
+#'   \item{Unit}{hospital name (ordered factor)}
+#'   \item{Satisfaction}{satisfaction mean (num)}
+#'   \item{Pain reduction}{pain reduction before and 1 year after op (num)}
+#'   \item{Gain in EQ5D}{gain in EQ5D-index before and 1 year after op (num)}
+#'   \item{AE90}{adverse events within 90 days (num)}
+#'   \item{Coverage}{coverage (num)}
+#'   \item{Reop 2 years}{reoperation within 2 years (num)}
+#'   \item{Implant 5 years}{implant survival within 5 years (num)}
+#'   \item{Implant 10 years}{implant survival within 10 years (num)}
+#' }
+#' @keywords SLR compass
+#' @name compass
+"compass_data"

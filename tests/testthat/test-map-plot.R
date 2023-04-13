d75ad4923ba6b971d9b37b30db95630d3773233c
Erik Bülow <eriklgb@gmail.com>
2023-04-13 11:43:26 +0200

    small fixes to pass checks

--- tests/testthat/test-map-plot.R
+++ tests/testthat/test-map-plot.R
@@ -3,6 +3,14 @@ test_that("map plot works", {
   # function at least returns a plot object
   expect_equal(
     c("gg", "ggplot"),
-    class(map_plot(df = counties, fill_var = "id"))
+    class(
+      map_plot(
+        df = counties,
+        fill_var = "id",
+        fill_colors = c(
+          RColorBrewer::brewer.pal(12, "Set3"),
+          RColorBrewer::brewer.pal(9, "Spectral"))
+        )
+      )
   )
 })

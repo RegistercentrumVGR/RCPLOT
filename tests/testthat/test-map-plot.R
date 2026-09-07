4299d3fef641fba6f681637e7cfed5ea6d356ff4
Albin Nydén <albny@rcvg.local>
2026-09-07 08:40:45 +0200

    test fix

--- tests/testthat/test-map-plot.R
+++ tests/testthat/test-map-plot.R
@@ -2,7 +2,7 @@ test_that("map plot works", {
   # Very simple testing to see that
   # function at least returns a plot object
   expect_equal(
-    c("ggplot", "gg"),
+    c("gg", "ggplot"),
     class(
       map_plot(
         df = counties,
